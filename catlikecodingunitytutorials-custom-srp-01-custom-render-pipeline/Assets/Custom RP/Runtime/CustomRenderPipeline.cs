ac0ff86e6f25e61738df5f8321980e04a763187d
zhyuli <zhyuli@tencent.com>
2021-10-21 20:10:27 +0800

    update

---
+++ catlikecodingunitytutorials-custom-srp-01-custom-render-pipeline/Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,16 @@
+﻿using MRender;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline {
+
+	MCameraRender renderer = new MCameraRender();
+
+	protected override void Render (
+		ScriptableRenderContext context, Camera[] cameras
+	) {
+		foreach (Camera camera in cameras) {
+			renderer.Render(context, camera);
+		}
+	}
+}
